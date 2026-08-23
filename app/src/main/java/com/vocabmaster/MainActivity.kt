bc4600e8d3db8ea81953fb7227999a236d8d4ad8
mshahrezaei <m.shaahrezaii@gmail.com>
2026-08-23 21:16:19 +0330

    Update MainActivity.kt

--- app/src/main/java/com/vocabmaster/MainActivity.kt
+++ app/src/main/java/com/vocabmaster/MainActivity.kt
@@ -1,1 +1,18 @@
+package com.vocabmaster
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.vocabmaster.ui.navigation.NavGraph
+import com.vocabmaster.ui.theme.VocabMasterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VocabMasterTheme {
+                NavGraph()
+            }
+        }
+    }
+}
